b5bbdc0988043a8247b8886cc835d86cd4499cd1
endarly <endarly@gmail.com>
2020-11-05 23:10:33 -0300

    agregar pages

---
+++ pages/cola_procesos_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class ColaProcesosPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    _colaprocesos = "//*[@class='sc-fzqLLg hjnSEH']//a[@href='/process_stack']"
+    _que_buscar = "//*[@id='search']"
+    _lupa = "//*[@class='sc-AxiKw fDcFlR']"
+    _documento_seleccionado = "//*[@id='81fcb830-147e-11eb-bf36-a59d44d12327']"
+    _path = "process_stack"
+    _ver = "//*[@href='/document/81fcb830-147e-11eb-bf36-a59d44d12327']"
+
+    def account_colaprocesos(self):
+        contrapartes = self.driver.find_element(By.XPATH,self._colaprocesos)
+        contrapartes.click()
+
+    def account_buscar(self):
+        quebuscar = self.driver.find_element(By.XPATH,self._que_buscar)
+        quebuscar.send_keys(Keys.TAB)
+        quebuscar.send_keys("DEFAULT DOXTOR")
+
+        lupa = self.driver.find_element(By.XPATH,self._lupa)
+        lupa.click()
+
+        contraparte_seleccionada= self.driver.find_element(By.XPATH, self._documento_seleccionado)
+        contraparte_seleccionada.click()
+
+        visualizar_documento = self.driver.find_element(By.XPATH, self._ver)
+        visualizar_documento.click()
